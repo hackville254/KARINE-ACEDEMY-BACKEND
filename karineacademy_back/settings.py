3cd01ace7539b8cff60904a5e28842dd6d5926b4
Franklin <fdelbo47@gmail.com>
2024-11-14 10:59:25 +0100

    DATA_UPLOAD_MAX_MEMORY_SIZE

--- karineacademy_back/settings.py
+++ karineacademy_back/settings.py
@@ -171,6 +171,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5 Mo, ajustez selon vos besoins
+FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5 Mo, ajustez selon vos besoins
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
