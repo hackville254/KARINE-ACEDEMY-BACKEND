7869e22a69a66714202ea80b0d2f57786a1c7aa5
Franklin <fdelbo47@gmail.com>
2024-11-17 03:37:05 +0100

    timeout=600

--- core/minio_utils.py
+++ core/minio_utils.py
@@ -16,7 +16,8 @@ minio_client = Minio(
     endpoint=settings.MINIO_ENDPOINT,
     access_key=settings.MINIO_ACCESS_KEY,
     secret_key=settings.MINIO_SECRET_KEY,
-    secure=settings.MINIO_USE_SSL
+    secure=settings.MINIO_USE_SSL,
+    timeout=600
 )
 
 
@@ -50,7 +51,7 @@ def delete_image_from_minio(file_url):
         print(f"Erreur lors de la suppression de l'image : {e}")
 
 
-def upload_video_to_minio(file, filename, content_type=None, retries=3, part_size=1 * 1024 * 1024):
+def upload_video_to_minio(file, filename, content_type=None, retries=3, part_size=10 * 1024 * 1024):
     """
     Uploads a large video file to MinIO with retry logic and chunked uploads.
     
